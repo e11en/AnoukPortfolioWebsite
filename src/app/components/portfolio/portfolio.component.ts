12a8045a330adac926a9a5da19a5b52d029fba88
elangelaar <ellenlangelaar@gmail.com>
2017-07-20 10:30:02 +0200

    Added thumnails and minor ajustment to the indexes

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -12,62 +12,52 @@ export class PortfolioComponent {
     selectedImageIndex: number;
 
     images = [
-        'anouk (1).JPG',
-        'anouk (2).JPG',
-        'anouk (3).JPG',
-        'anouk (4).JPG',
-        'anouk (5).JPG',
-        'anouk (6).JPG',
-        'anouk (7).JPG',
-        'anouk (8).JPG',
-        'anouk (9).JPG',
-        'anouk (10).JPG',
-        'anouk (11).JPG',
-        'anouk (12).JPG',
-        'anouk (13).JPG',
-        'anouk (14).JPG',
-        'anouk (15).JPG',
-        'anouk (16).JPG',
-        'anouk (17).JPG',
-        'anouk (18).JPG',
-        'anouk (19).JPG',
-        'anouk (20).JPG',
-        'anouk (21).JPG',
-        'anouk (22).JPG',
-        'anouk (23).JPG',
-        'anouk (24).JPG',
-        'anouk (25).JPG',
-        'anouk (26).JPG',
-        'anouk (27).JPG',
-        'anouk (28).JPG',
-        'anouk (29).JPG',
-        'anouk (30).JPG',
-        'anouk (31).JPG',
-        'anouk (32).JPG',
-        'anouk (33).JPG',
-        'anouk (34).JPG',
-        'anouk (35).JPG',
-        'anouk (36).JPG',
-        'anouk (37).JPG'
+        'anouk (1).jpg',
+        'anouk (2).jpg',
+        'anouk (3).jpg',
+        'anouk (4).jpg',
+        'anouk (5).jpg',
+        'anouk (6).jpg',
+        'anouk (7).jpg',
+        'anouk (8).jpg',
+        'anouk (9).jpg',
+        'anouk (10).jpg',
+        'anouk (11).jpg',
+        'anouk (12).jpg',
+        'anouk (13).jpg',
+        'anouk (14).jpg',
+        'anouk (15).jpg',
+        'anouk (16).jpg',
+        'anouk (17).jpg',
+        'anouk (18).jpg',
+        'anouk (19).jpg',
+        'anouk (20).jpg',
+        'anouk (21).jpg',
+        'anouk (22).jpg',
+        'anouk (23).jpg',
+        'anouk (24).jpg',
+        'anouk (25).jpg',
+        'anouk (26).jpg',
+        'anouk (27).jpg',
+        'anouk (28).jpg',
+        'anouk (29).jpg',
+        'anouk (30).jpg',
+        'anouk (31).jpg',
+        'anouk (32).jpg',
+        'anouk (33).jpg',
+        'anouk (34).jpg',
+        'anouk (35).jpg',
+        'anouk (36).jpg',
+        'anouk (37).jpg'
     ];
-
-    openModal(index) {
-        this.setSelectedImage(index);
-        this.toggleModal();
-    }
 
     setSelectedImage(index) {
         this.selectedImageIndex = index;
-        this.selectedImage = this.images[index];
-    }
-
-    toggleModal() {
-        $('.overlay').toggleClass('hidden');
-        $('.modal').toggleClass('hidden');
+        this.selectedImage = this.images[index - 1];
     }
 
     previous() {
-        if ((this.selectedImageIndex - 1) >= 0) {
+        if ((this.selectedImageIndex - 1) > 0) {
             this.setSelectedImage(this.selectedImageIndex - 1);
         }
     }
